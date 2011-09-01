d6d51ca515cb131086893781cacb917bd3886cf0
unknown <rtuin@.(none)>
2011-09-01 21:07:06 +0200

    Added transition effects and some bogus text

--- js/webpresent.js
+++ js/webpresent.js
@@ -9,18 +9,31 @@
             var el = $(this);
             el.addClass('next').css('zIndex', zIndex--);
         });
+        $('.item').addClass('hidden');
     });
     $(document).keydown(function (event) {
         switch (event.keyCode) {
-            case 39:
-                console.log('next');
+            case 39: // next
                 var current = $('section.current');
+                var items = current.find('.item.hidden');
+                if (items.length) {
+                    items.first().removeClass('hidden');
+                    break;
+                }
+
                 if (!current.next('section').length) return;
                 current.next('section').removeClass('next').addClass('current');
                 current.addClass('previous').removeClass('current');
                 break;
-            case 37:
+            case 37: // previous
                 var current = $('section.current');
+
+                var items = current.find('.item:not(.hidden)');
+                if (items.length) {
+                    items.last().addClass('hidden');
+                    break;
+                }
+
                 if (!current.prev('section').length) return;
                 current.prev('section').removeClass('previous').addClass('current');
                 current.addClass('next').removeClass('current');
