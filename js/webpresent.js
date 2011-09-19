3825f245d181e306422c9960858b5a6c549a2edd
Richard Tuin <richardtuin@gmail.com>
2011-09-19 22:25:10 +0200

    Added support for control via mouse

--- js/webpresent.js
+++ js/webpresent.js
@@ -3,6 +3,7 @@
 // webpresent.js may be freely distributed, altered, used and abused under the who-needs-a license
 (function($) {
     $(document).ready(function() {
+        $(this).bind('contextmenu', function() { return false; });
         $('section:first-child').addClass('current');
         var zIndex = 500;
         $('section:not(.current)').each(function() {
@@ -15,31 +16,52 @@
         switch (event.keyCode) {
             case 39: // next
             case 34: // next
-                var current = $('section.current');
-                var items = current.find('.item.hidden');
-                if (items.length) {
-                    items.first().removeClass('hidden');
-                    break;
-                }
-
-                if (!current.next('section').length) return;
-                current.next('section').removeClass('next').addClass('current');
-                current.addClass('previous').removeClass('current');
+                next();
                 break;
             case 37: // previous
             case 33: // previous
-                var current = $('section.current');
-
-                var items = current.find('.item:not(.hidden)');
-                if (items.length) {
-                    items.last().addClass('hidden');
-                    break;
-                }
-
-                if (!current.prev('section').length) return;
-                current.prev('section').removeClass('previous').addClass('current');
-                current.addClass('next').removeClass('current');
+                previous();
                 break;
         }
     });
+    $(document).mousedown(function(event) {
+        switch(event.which) {
+            case 1:
+                next();
+                break;
+            case 3:
+            {
+                console.log('??');
+                previous();
+                break;
+            }
+        }
+    });
+
+    function next() {
+        var current = $('section.current');
+        var items = current.find('.item.hidden');
+        if (items.length) {
+            items.first().removeClass('hidden');
+            return;
+        }
+
+        if (!current.next('section').length) return;
+        current.next('section').removeClass('next').addClass('current');
+        current.addClass('previous').removeClass('current');
+    }
+
+    function previous() {
+        var current = $('section.current');
+
+        var items = current.find('.item:not(.hidden)');
+        if (items.length) {
+            items.last().addClass('hidden');
+            return;
+        }
+
+        if (!current.prev('section').length) return;
+        current.prev('section').removeClass('previous').addClass('current');
+        current.addClass('next').removeClass('current');
+    }
 })(jQuery);
